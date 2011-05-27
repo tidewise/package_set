13486b5cfc6ed33c55ad60bfdd0f8527e38f80e5
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2011-05-27 17:20:39 +0200

    limit the possibility to push to the next or stable branches by accident

--- overrides.rb
+++ overrides.rb
@@ -1,3 +1,24 @@
+# We want to avoid accidents where stuff gets pushed to next or stable that was
+# not intended to be there
+#
+# We therefore set the push_to_branch to master. This makes sure that
+#
+#   git push
+#   git push autobuild
+#
+# pushes to master instead of the local branch
+Autoproj.manifest.each_package do |pkg|
+    if pkg.importer.kind_of?(Autobuild::Git)
+        if !pkg.importer.push_to_branch
+            if pkg.importer.branch == "next" || pkg.importer.branch == "stable"
+                pkg.importer.push_to_branch = "master"
+            else
+                pkg.importer.push_to_branch = pkg.importer.branch
+            end
+        end
+    end
+end
+
 if Autoproj.respond_to?(:post_import)
     # Override the CMAKE_BUILD_TYPE configuration parameter based on the
     # "stable" tag
