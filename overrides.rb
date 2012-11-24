ca147d9b4cc18e033dc51244f175901f6407fcb9
Sylvain Joyeux <sylvain.joyeux@dfki.de>
2012-11-24 07:55:34 +0100

    better handling of OCL
     * checkout the master branch when in master flavor
     * don't exclude it from the build. Handle it properly instead.

--- overrides.rb
+++ overrides.rb
@@ -1,5 +1,9 @@
 Autoproj.manifest.each_autobuild_package do |pkg|
-    if pkg.kind_of?(Autobuild::Orogen) && pkg.name != "tools/logger" && pkg.name != "base/orogen/types"
+    next if !pkg.kind_of?(Autobuild::Orogen)
+    if pkg.name != "tools/logger" && pkg.name != "base/orogen/types"
         pkg.optional_dependency 'tools/logger'
+    end
+    if !Autoproj.user_config('USE_OCL')
+        pkg.optional_dependencies.delete 'ocl'
     end
 end
