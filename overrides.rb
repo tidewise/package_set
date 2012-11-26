a3ce1b2be52e50e8c438d182613e4e11ec440422
Sylvain Joyeux <sylvain.joyeux@dfki.de>
2012-11-26 16:35:53 +0100

    add missing optional dependency on tools/service_discovery

--- overrides.rb
+++ overrides.rb
@@ -3,6 +3,7 @@ Autoproj.manifest.each_autobuild_package do |pkg|
     if pkg.name != "tools/logger" && pkg.name != "base/orogen/types"
         pkg.optional_dependency 'tools/logger'
     end
+    pkg.optional_dependency 'tools/service_discovery'
     if !Autoproj.user_config('USE_OCL')
         pkg.optional_dependencies.delete 'ocl'
     end
