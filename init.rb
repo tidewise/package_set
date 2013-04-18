b32af6f17cd2c5c4ee9339f6122fa6f4e65507a9
Sylvain Joyeux <sylvain.joyeux@dfki.de>
2013-04-18 17:24:13 +0200

    forcefully unset ROS_ROOT

--- init.rb
+++ init.rb
@@ -1,4 +1,1 @@
-if ENV['ROS_ROOT'] || ENV['ROS_PATH']
-    Autoproj.error "you cannot build Rock with ROS_ROOT / ROS_PATH set. Unset it and try again"
-    exit 1
-end
+Autoproj.env_unset 'ROS_ROOT'
