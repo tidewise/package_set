b8defac95b7f455c6c4bd2246550cc8afbfdf089
Sylvain Joyeux <sylvain.joyeux@dfki.de>
2013-05-13 13:18:47 +0200

    add base/orogen/std as an optional dependency of all orogen packages

--- overrides.rb
+++ overrides.rb
@@ -3,6 +3,10 @@ Autoproj.manifest.each_autobuild_package do |pkg|
     if pkg.name != "tools/logger" && pkg.name != "base/orogen/types" && pkg.name != 'base/orogen/std'
         pkg.optional_dependency 'tools/logger'
     end
+    if pkg.name != 'base/orogen/std'
+        pkg.optional_dependency 'base/orogen/std'
+    end
+
     pkg.optional_dependency 'tools/service_discovery'
     if !Autoproj.user_config('USE_OCL')
         pkg.optional_dependencies.delete 'ocl'
