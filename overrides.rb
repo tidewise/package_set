a59e291096a9f863c0437893bf04e294edc509cb
Sylvain Joyeux <sylvain.joyeux@dfki.de>
2013-10-11 16:14:05 +0200

    base/orogen/std is available in stable, make use of it

--- overrides.rb
+++ overrides.rb
@@ -3,7 +3,7 @@ Autoproj.manifest.each_autobuild_package do |pkg|
     if pkg.name != "tools/logger" && pkg.name != "base/orogen/types" && pkg.name != 'base/orogen/std'
         pkg.optional_dependency 'tools/logger'
     end
-    only_in_flavor 'master','next' do
+    only_in_flavor 'master','next', 'stable' do
         if pkg.name != 'base/orogen/std'
             pkg.optional_dependency 'base/orogen/std'
             pkg.orogen_options << '--import=std'
