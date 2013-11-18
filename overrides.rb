3ae00cc3c4ddc6873ad29bac764594f913696c20
Matthias Goldhoorn <matthias.goldhoorn@dfki.de>
2013-11-18 14:59:06 +0100

    workarounded buggy only_in_flavor statement for metaata support

--- overrides.rb
+++ overrides.rb
@@ -3,8 +3,10 @@ Autoproj.manifest.each_autobuild_package do |pkg|
     if pkg.name != "tools/logger" && pkg.name != "base/orogen/types" && pkg.name != 'base/orogen/std'
         pkg.optional_dependency 'tools/logger'
     end
-    only_in_flavor 'master' do
-            pkg.orogen_options << '--extensions=metadata_support'
+
+    if Autoproj.user_config('ROCK_FLAVOR') == 'master'
+    #BUGGY: only_in_flavor 'master' do 
+        pkg.orogen_options << '--extensions=metadata_support'
     end
     only_in_flavor 'master','next', 'stable' do
         if pkg.name != 'base/orogen/std'
