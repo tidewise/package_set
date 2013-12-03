b66cb559e49a170b058bb279cedcd230f2f4c41d
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2013-12-03 14:50:07 +0100

    properly configure the https access to github

--- init.rb
+++ init.rb
@@ -18,7 +18,7 @@ if !Autoproj.has_source_handler? 'gitorious'
     Autoproj.gitorious_server_configuration('GITORIOUS', 'gitorious.org')
 end
 if !Autoproj.has_source_handler? 'github'
-    Autoproj.gitorious_server_configuration('GITHUB', 'github.com')
+    Autoproj.gitorious_server_configuration('GITHUB', 'github.com', :http_url => 'https://github.com')
 end
 
 FLAVORS = Hash.new
